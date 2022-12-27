588436f9b810c1d41416397d2fdea707574b7e58
AgarkovDmitry <dioxs.7z@gmail.com>
2022-12-27 15:57:44 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-jnapvl
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-jnapvl)
---
+++ src/widgets/Widget.tsx
@@ -0,0 +1,12 @@
+import Logos from '../components/Logos';
+import '../App.css';
+
+function Widget() {
+  return (
+    <div className="App">
+      <Logos />
+    </div>
+  );
+}
+
+export default Widget;
